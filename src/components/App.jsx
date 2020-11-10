159aea41f7408fba0dfd704ee2878d34edd6deef
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2020-11-10 11:36:15 -0500

    hooks added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [entry, setEntry] = useState({
+    title: "",
+    content: ""
+  });
+
+  function addEntry(evt) {
+    let { name, value } = evt.target;
+    setEntry((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea addEntry={addEntry} entryValue={entry} />
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  function addKeeper(evt) {
+    props.addEntry(evt);
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form onChange={addKeeper}>
+        <input
+          value={props.entryValue.title}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          value={props.entryValue.content}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button>Add</button>
       </form>
     </div>
